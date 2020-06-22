a647b17a199ef317022aa3496bdf6ab4d5c29219
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-06-22 20:08:38 +0600

    feat: added JS libs and filter and slider, this is latest commit :(

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-const menuIconWrapper = document.querySelector('.menu-icon-wrapper');
+$(document).ready(function () {
+  const menuIconWrapper = $('.menu-icon-wrapper');
 
-menuIconWrapper.addEventListener('click', () => {
-  document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+  // burger-menu
+  menuIconWrapper.on('click', function () {
+    $('.menu-icon').toggleClass('menu-icon-active');
+  });
+  // slider
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 10000,
+    loop: true,
+    items: 1,
+  });
+  // smooth scroll
+  function smoothScrollHome() {
+    $('#js-arrow').on('click', function (event) {
+      event.preventDefault();
+      let top = $('#js-top').offset().top;
+
+      $('html, body').animate({
+          scrollTop: top,
+        },
+        500
+      );
+    });
+  }
+
+  smoothScrollHome();
+
+  //mixitup
+  let mixer = mixitup('.products__row');
 });
